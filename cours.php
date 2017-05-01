2a173ba1aa63701e6e28a3629e8f2851a2cc1fc9
xmol31 <l.taillez@gmail.com>
2017-05-01 17:10:32 +0200

    cours sur les variables

--- cours.php
+++ cours.php
@@ -12,6 +12,26 @@
 
 <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
 
+    <p>
+        <?php
+    
+    $ageduvisiteur = 24;
+     echo 'Le visiteur a ' . $ageduvisiteur . ' ans'; 
+    ?>
+    </p>
+    
+   <p>
+       <?php
+    $prix = 2.5; //mettre un . et non une ,
+    $quantite= 3;
+    $total= $prix * $quantite; /* la base des variables */
+    echo 'Le prix des tomates est de ' . $total . ' euros.';
+   
+    
+       ?> 
+    </p>
+    
+
 </body>
 
 </html>
