78f04fc14b444189de08504cbfedd7c74ee8907b
xmol31 <l.taillez@gmail.com>
2017-05-01 16:09:44 +0200

    first commit

---
+++ cours.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset= "utf-8"
+<title> Cours Php </title>
+</head>
+
+<body>
+
+<h1 style= "color: blue";> Mon Site Web</h1>
+
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+</body>
+
+</html>
