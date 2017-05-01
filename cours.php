444022e664fd0c3ce64986a941faa833eafbfa22
xmol31 <l.taillez@gmail.com>
2017-05-01 18:57:38 +0200

    cours sur les conditions

--- cours.php
+++ cours.php
@@ -2,36 +2,42 @@
 <html>
 
 <head>
-<meta charset= "utf-8"
+<meta charset= "utf-8">
+
+
+    
 <title> Cours Php </title>
 </head>
 
 <body>
 
-<h1 style= "color: blue";> Mon Site Web</h1>
+<h1> Mon Site Web</h1>
 
-<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
 
-    <p>
-        <?php
-    
-    $ageduvisiteur = 24;
-     echo 'Le visiteur a ' . $ageduvisiteur . ' ans'; 
-    ?>
-    </p>
-    
-   <p>
+
+  
+
        <?php
-    $prix = 2.5; //mettre un . et non une ,
-    $quantite= 3;
-    $total= $prix * $quantite; /* la base des variables */
-    echo 'Le prix des tomates est de ' . $total . ' euros.';
-   
     
-       ?> 
-    </p>
+    $age = 19; //changer pour voir la condition s'appliquer
     
+if ($age <= 17)
+{
+ ?> 
+    <p style= "color: blue";> Salut jeune! </p>
+    <?php
+}
+    elseif ($age==18)
+    {
+        echo ' Tu as pile l\'age ^^'; // le \ sert à mettre le ' 
 
+}
+    else
+    {
+        echo 'Bonjour Adulte!';
+    }
+
+?>
 </body>
 
 </html>
